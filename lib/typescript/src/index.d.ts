8cc769d7540f625e188763d8c14ea5626421eb24
Jakub Dudek <jakub@dudek.dev>
2026-04-30 10:06:24 +0200

    fix: missing TS interface for setTheme function

--- lib/typescript/src/index.d.ts
+++ lib/typescript/src/index.d.ts
@@ -2,8 +2,19 @@ interface ParakeyInterface {
     configure(tokenBundle: string): Promise<void>;
     deconfigure(): Promise<void>;
     showScan(): Promise<void>;
+    setTheme(hexColors: {
+        actionLight?: string;
+        actionDark?: string;
+        titleLight?: string;
+        titleDark?: string;
+    }): Promise<void>;
 }
 declare const Parakey: ParakeyInterface;
 export default Parakey;
-export declare const configure: (tokenBundle: string) => Promise<void>, deconfigure: () => Promise<void>, showScan: () => Promise<void>;
+export declare const configure: (tokenBundle: string) => Promise<void>, deconfigure: () => Promise<void>, showScan: () => Promise<void>, setTheme: (hexColors: {
+    actionLight?: string;
+    actionDark?: string;
+    titleLight?: string;
+    titleDark?: string;
+}) => Promise<void>;
 //# sourceMappingURL=index.d.ts.map
